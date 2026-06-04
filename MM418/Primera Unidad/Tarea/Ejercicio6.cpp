ddebbb452b8440a408f03744f65b1315a62b1fac
IvanHHdz <hernivan336@outlook.com>
2026-06-04 08:23:13 -0600

    Tarea de MM418
    Ejercicio 6 de la primera tarea

--- MM418/Primera Unidad/Tarea/Ejercicio6.cpp
+++ MM418/Primera Unidad/Tarea/Ejercicio6.cpp
@@ -1,11 +1,42 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 using namespace std;
 
+// esta función llena el arreglo con números aleatorios
+void llenar_arr(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    arr[i] = 1 + rand() % 100;
+  }
+}
+
+void imprimir_matriz_ti(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i >= j) {
+        cout << arr[(i * (i + 1)) / 2 + j];
+      } else {
+        cout << 0;
+      }
+      cout << '\t';
+    }
+    cout << endl;
+  }
+}
+
 int main() {
-    int n;
+  srand(time(0));
+  int n, s;
 
-    cout << "Ingrese el tamaño de una matriz cuadrada: ";
+  cout << "Ingrese el tamaño de una matriz cuadrada: ";
+  cin >> n;
 
+  s = (n * (n + 1)) / 2;
+  int *matriz_t = new int[s];
 
-    return 0;
+  llenar_arr(matriz_t, s);
+
+  imprimir_matriz_ti(matriz_t, n);
+
+  return 0;
 }
--- MM418/Primera Unidad/Tarea/README.md
+++ MM418/Primera Unidad/Tarea/README.md
@@ -74,7 +74,9 @@ imprima los siguiente:
 Diseña un programa en C++ que construya una matriz triangular inferior utilizando memoria dinámica:
 
 1. Solicitar al usuario el tamaño n de la matriz cuadrada.
-2. Reservar memoria dinámica solo para los elementos necesarios de la triangular inferior (es decir, donde 𝑖 ≤ 𝑗).
+2. Reservar memoria dinámica solo para los elementos necesarios de la triangular inferior 
+(es decir, donde 𝑖 ≤ 𝑗).
 3. genere los datos de manera aleatoria.
-4. Mostrar la matriz completa en forma cuadrada, colocando ceros en las posiciones que no pertenecen a la parte triangular
+4. Mostrar la matriz completa en forma cuadrada, colocando ceros en las posiciones que no 
+pertenecen a la parte triangular
 5. Liberar toda la memoria al finalizar.
